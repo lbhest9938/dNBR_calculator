d62148f83c86504bd6d4b0daca230b2b1fb4de9d
Luke Hester (LBHEST9938) <LBHEST9938@ung.edu>
2018-11-28 17:02:21 -0500

    description of function steps and the purpose     Please enter the commit message for your changes. Lines starting

--- dNBR.py
+++ dNBR.py
@@ -3,3 +3,21 @@
 ##this project is also designed for python 3.6
 ##first we must install git, bash, and anaconda to local computer
 ##after those are installed we need to install earth-analytics-python environment
+
+#** means that we are missing a step here and need to figure it out
+
+##this program does the following in this order
+
+##1. opens pre fire landsat data and calculates the NBR of prefire
+
+##2. opens post fire data and calculates NBR of postfire
+
+##3. calculates dNBR using simple rater functions
+
+##4. classifies fire damage using certain value ranges in dNBR
+
+##5. calculates areas of no burn, low, moderate, and high fire damage.
+
+##6. generates color map of dNBR
+
+##7. plots these classes in a graph
