6515b61eacad2f1f2047104b65989122be26a03f
Luke Hester (LBHEST9938) <LBHEST9938@ung.edu>
2018-11-28 16:54:21 -0500

    simple description of function

--- dNBR.py
+++ dNBR.py
@@ -1,1 +1,5 @@
 
+#final project for python
+##this project is also designed for python 3.6
+##first we must install git, bash, and anaconda to local computer
+##after those are installed we need to install earth-analytics-python environment
